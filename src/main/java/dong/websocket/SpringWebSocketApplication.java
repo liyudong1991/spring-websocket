6abcf0a52f030b37d28e31fb45352892a8d1185d
liyudong <1768756152@qq.com>
2019-05-16 17:33:31 +0800

    init

---
+++ src/main/java/dong/websocket/SpringWebSocketApplication.java
@@ -0,0 +1,16 @@
+package dong.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liyd
+ */
+@SpringBootApplication
+public class SpringWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebSocketApplication.class, args);
+    }
+
+}
